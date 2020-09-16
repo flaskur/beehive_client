c57417a9de02d8eae086a44c3df8629d982e4eb0
flaskur <flaskur@gmail.com>
2020-09-16 12:33:01 -0400

    render home or login page based on auth state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.scss';
 import { Switch, Route } from 'react-router-dom';
 import Home from './pages/home/Home';
 import Login from './pages/login/Login';
 import Navbar from './components/navbar/Navbar';
+import About from './components/about/About';
 
 const App = () => {
+	const [ isAuth, setIsAuth ] = useState(false);
+
+	// on initial load, check if jwt exists in local storage to authenticate
+	useEffect(() => {
+		const localToken = localStorage.getItem('token');
+		if (!localToken) return;
+
+		setIsAuth(true);
+	}, []);
+
 	return (
 		<div className='app'>
 			<p>This is a test</p>
@@ -13,10 +24,10 @@ const App = () => {
 
 			<Switch>
 				<Route exact path='/'>
-					<Home />
+					{isAuth ? <Home /> : <Login />}
 				</Route>
-				<Route exact path='/login'>
-					<Login />
+				<Route exact path='/about'>
+					<About />
 				</Route>
 			</Switch>
 		</div>
