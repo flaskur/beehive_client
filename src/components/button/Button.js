32c471ae1507a439f16ea7287ac5a1f97853f3e7
flaskur <flaskur@gmail.com>
2020-09-16 18:45:03 -0400

    setup login form scaffold

---
+++ src/components/button/Button.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Button.scss';
+
+const Button = ({ text, handler }) => {
+	return (
+		<button className='button' onClick={handler}>
+			{text}
+		</button>
+	);
+};
+
+export default Button;
