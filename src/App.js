e34927d743330cbe03fb5b4d16df772ebe295e86
flaskur <flaskur@gmail.com>
2020-09-16 12:13:01 -0400

    added initial routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import './App.scss';
+import { Switch, Route } from 'react-router-dom';
+import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+import Navbar from './components/navbar/Navbar';
 
 const App = () => {
 	return (
 		<div className='app'>
 			<p>This is a test</p>
+			<Navbar />
+
+			<Switch>
+				<Route exact path='/'>
+					<Home />
+				</Route>
+				<Route exact path='/login'>
+					<Login />
+				</Route>
+			</Switch>
 		</div>
 	);
 };
