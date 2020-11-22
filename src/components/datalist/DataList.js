0cad77189d85e52d8094d757760cae28e4393465
flaskur <flaskur@gmail.com>
2020-11-22 09:30:57 -0500

    data list progress

--- src/components/datalist/DataList.js
+++ src/components/datalist/DataList.js
@@ -6,13 +6,25 @@ const DataList = ({data: {accessor, redfin}}) => {
 	console.log(redfin)
 
 	const getAccessorContent = (accessor) => {
-		console.log('invokes')
 		let content = [];
 		for (let key in accessor) {
 			content.push(
-				<div>
+				<div class='datalist__row'>
 					<p>{key}</p>
 					<p>{accessor[key]}</p>
+				</div>
+			);
+		}
+
+		return content;
+	};
+	const getRedfinContent = (redfin) => {
+		let content = [];
+		for (let key in redfin) {
+			content.push(
+				<div class='datalist__row'>
+					<p>{key}</p>
+					<p>{redfin[key]}</p>
 				</div>
 			);
 		}
@@ -25,8 +37,15 @@ const DataList = ({data: {accessor, redfin}}) => {
 		<div>
 			<p>Data List</p>
 			{/* <p>{JSON.stringify(accessor)}</p> */}
+			<div>
+				<h1>ACCESSOR SHOULD BE DYNAMIC</h1>
+				<div class='datalist__accessor'>{getAccessorContent(accessor)}</div>
+			</div>
 
-			<div>{getAccessorContent(accessor)}</div>
+			<div>
+				<h1>REDFIN</h1>
+				<div class='datalist__redfin'>{getRedfinContent(redfin)}</div>
+			</div>
 			
 			{/* <p>{JSON.stringify(redfin)}</p> */}
 		</div>
